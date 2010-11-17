c6598a0e1e63bd52cb6509e41cd5ed01a5fd3441
Alex Price <alex@Alexs-Laptop.(none)>
2010-11-17 21:01:31 +0000

    Created a few notification classes ready for adding code

---
+++ Framework/Libraries/Notifications/Notification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modula\Framework\Notifications;
+
+/**
+ * @ingroup application
+ */
+class Notification extends \Modula\Framework\Object {
+
+  private $bindAttributes = array();
+  private $message;
+  private $expireTime;
+
+  public function __construct(){
+
+  }
+
+  public function getMessage(){
+
+  }
+
+  public function getExpireTime(){
+
+  }
+
+  public function getAttribute($name){
+
+  }
+
+}
+
+?>
---
+++ Framework/Libraries/Notifications/NotificationManager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modula\Framework\Notifications;
+
+/**
+ * @ingroup application
+ */
+class NotificationManager extends \Modula\Framework\Object {
+
+  public static function addNotification(Notification $notification){
+
+  }
+
+  public static function deleteNotification(Notification $notification){
+
+  }
+
+  public static function getNotifications(){
+
+  }
+
+}
+
+?>
