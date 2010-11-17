b0cb77e697056347dfe7326dedd8d6c577441653
Alex Price <alex@Alexs-Laptop.(none)>
2010-11-18 05:01:31 +0800

    Created a few notification classes ready for adding code

---
+++ Framework/Libraries/Notifications/NotificationRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modula\Framework\Notifications;
+
+/**
+ * @ingroup application
+ */
+class NotificationRepository extends \Modula\Framework\Object {
+
+  public function create(){
+
+  }
+
+  public function read(){
+
+  }
+
+  public function update(){
+
+  }
+
+  public function delete(){
+
+  }
+
+}
+
+?>
